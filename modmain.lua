bea7825a291271c94a66478a3ac0f595df2a74ba
DSTBoas <60602264+DSTBoas@users.noreply.github.com>
2020-02-03 18:46:58 +0100

    Update modmain.lua

--- modmain.lua
+++ modmain.lua
@@ -21,32 +21,32 @@ local util =
 }
 
 local function GetFormFromHunger(hunger, lastform)
-    local mighty = lastform == 3 and 220 or GLOBAL.TUNING.WOLFGANG_START_MIGHTY_THRESH
-    local wimpy = lastform == 1 and 105 or GLOBAL.TUNING.WOLFGANG_START_WIMPY_THRESH
-    if hunger > mighty then
-        return 3
-    elseif hunger <= wimpy then
-        return 1
-    else
-        return 2
-    end
+	local mighty = lastform == 3 and 220 or GLOBAL.TUNING.WOLFGANG_START_MIGHTY_THRESH
+	local wimpy = lastform == 1 and 105 or GLOBAL.TUNING.WOLFGANG_START_WIMPY_THRESH
+	if hunger > mighty then
+		return 3
+	elseif hunger <= wimpy then
+		return 1
+	else
+		return 2
+	end
 end
 
 local function OnHungerDelta(inst, data)
 	local hunger = inst.replica.hunger:GetCurrent()
 	currentForm = GetFormFromHunger(hunger, currentForm)
-    if currentForm == 1 then return end
-    if (hunger - warning) <= util.deform[currentForm] then
-        GLOBAL.ThePlayer.components.talker:Say(string.format("Wolfgang becomes %s in %d hunger.", util.strings[currentForm-1], (hunger - util.deform[currentForm])))
-    end
+    	if currentForm == 1 then return end
+    	if (hunger - warning) <= util.deform[currentForm] then
+		GLOBAL.ThePlayer.components.talker:Say(string.format("Wolfgang becomes %s in %d hunger.", util.strings[currentForm-1], (hunger - util.deform[currentForm])))
+    	end
 end
 
 local function Init(inst)
 	if inst.prefab ~= "wolfgang" then return end
-    inst:DoTaskInTime(0, function()
+	inst:DoTaskInTime(0, function()
 	   currentForm = GetFormFromHunger(inst.replica.hunger:GetCurrent())
 	   inst:ListenForEvent("hungerdelta", OnHungerDelta)
-    end)
+	 end)
 end
 
 AddPlayerPostInit(Init)
