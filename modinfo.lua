4502728c69a2efdc9fb0946a8d19b13b36cdc461
Boas van der Hoeven <BoasHoeven+DST@gmail.com>
2020-05-09 12:44:49 +0200

    Improve config

--- modinfo.lua
+++ modinfo.lua
@@ -1,50 +1,71 @@
 name = "Wolfgang Main"
-author = "Boas"
-version = "2.0"
-
-forumthread = ""
 description = "Wolfgang warns you when you are about to change form"
-
-api_version = 10
 
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 
+author = "Boas"
+version = "2.1"
+forumthread = ""
+
 dont_starve_compatible = false
 reign_of_giants_compatible = false
-shipwrecked_compatible = false
 dst_compatible = true
 
 all_clients_require_mod = false
 client_only_mod = true
-server_filter_tags = {}
 
-local warning = {}
-local bool =
+api_version = 10
+
+folder_name = folder_name or "Wolfgang-Main"
+if not folder_name:find("workshop-") then
+    name = name .. " (dev)"
+end
+
+local function AddConfig(label, name, options, default, hover)
+    return {
+                label = label,
+                name = name,
+                options = options,
+                default = default,
+                hover = hover or ""
+           }
+end
+
+local function AddSectionTitle(title)
+    return AddConfig(title, "", {{description = "", data = 0}}, 0)
+end
+
+local function CreateOption(desc, data, hover)
+    return {description = desc, data = data, hover = hover}
+end
+
+local SettingOptions =
 {
-    {description = "Enabled", data = true},
-    {description = "Disabled", data = false},
+    CreateOption("Enabled", true, "Warning messages have a color gradient"),
+    CreateOption("Disabled", false),
 }
+
+local HungerOptions = {}
 
 for i = 0, 11 do
     local hunger = 3 + i * 2
     hunger = (hunger + 1) % 10 == 0 and hunger + 1 or hunger
     if (hunger - 1) % 10 ~= 0 then
-        warning[#warning + 1] =
-        {
-            description = hunger.."",
-            data = hunger,
-            hover = "Warning starts at " .. (100 + hunger) .. " and " .. (220 + hunger) .. " hunger"
-        }
+        local hover = "Warning starts at " .. (100 + hunger) .. " and " .. (220 + hunger) .. " hunger"
+        HungerOptions[#HungerOptions + 1] = CreateOption(
+                                                hunger.."", 
+                                                hunger,
+                                                hover
+                                            )
     end
 end
 
-local function AddConfig(label, name, options, default, hover)
-    return {label = label, name = name, options = options, default = default, hover = hover or ""}
-end
+local SettingMessage = "Set to your liking"
 
 configuration_options =
 {
-    AddConfig("Amount of hunger", "WARNING", warning, 5, "Amount of hunger"),
-    AddConfig("Color", "COLORED", bool, false, "Warning messages have a color gradient"),
+    AddSectionTitle("Settings"),
+    AddConfig("Hunger", "WARNING", HungerOptions, 5, SettingMessage),
+    AddConfig("Color", "COLORED", SettingOptions, false, SettingMessage)
 }
